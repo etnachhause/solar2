b3398ef45794e4d62eda09c864f57dba3407eabc
etnachhause <51200957+etnachhause@users.noreply.github.com>
2023-06-29 11:31:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 var filesToCache = [
   '.',
   '/',
-  'index.html',
-  'manifest.json',
-  'service-worker.js',
-  'bilder/favicon.ico',
-  'bilder/sonne.png',
-  'bilder/euro.png',
-  'bilder/sonne.png',
-  'bilder/sonne144.png',
-'bilder/touch-icon-iphone.png'	
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  '/bilder/favicon.ico',
+  '/bilder/sonne.png',
+  '/bilder/euro.png',
+  '/bilder/sonne.png',
+  '/bilder/sonne144.png',
+'/bilder/touch-icon-iphone.png'	
 ];
 
 	latestCacheName = 'App-Shell-v1';
@@ -23,30 +23,21 @@ var filesToCache = [
 	  );
 	});
 
-	self.addEventListener('activate', event => {
-	  event.waitUntil(
-		caches.keys().then(allCaches => {
-			allCaches.forEach(cache => {
-			  if (cache !== latestCacheName) {
-				  caches.delete(cache);
-			  }
-			})
-		})
-	  );
-	});
+	
 
 	self.addEventListener("fetch", event => {
     if (event.request.url === "https://www.etnachhause.github.io/solar2.index.html") {
         // or whatever your app's URL is
         event.respondWith(
             fetch(event.request).catch(err =>
-                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
+                self.cache.open(cache_name).then(cache => cache.match("/index.html"))
             )
         );
     } else {
         event.respondWith(
             fetch(event.request).catch(err =>
-                caches.match(event.request).then(response => response)
+                caches.match(event.request).then((response) => { 
+ 		return response || fetch(event.request); 
             )
         );
     }
